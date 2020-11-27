c8ea338d4faf0b497d064c10c503a439dee95074
shiniao <zhuzhezhe95@gmail.com>
2020-11-27 16:57:12 +0800

    add ticker learn

---
+++ go/ticker.go
@@ -0,0 +1,30 @@
+package learn
+
+import (
+	"fmt"
+	"time"
+)
+
+// Ticker 代表一个ticker
+func Ticker() {
+	// 设置一个一秒的ticker
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	done := make(chan bool)
+
+	go func() {
+		// 十秒之后结束
+		time.Sleep(10 * time.Second)
+		done <- true
+	}()
+
+	for {
+		select {
+		case t := <-ticker.C:
+			fmt.Println("current time: ", t)
+		case <-done:
+			fmt.Println("Done!")
+			return
+		}
+	}
+}
